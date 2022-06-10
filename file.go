f738e453088c2d9d976b5e5e36f73f41f3b82737
张复星 <zhangfuxing@cmict.chinamobile.com>
2022-06-10 14:27:34 +0800

    add FileExists

---
+++ file.go
@@ -0,0 +1,12 @@
+package tool
+
+import "os"
+
+// FileExists check if a file exists
+func FileExists(filepath string) bool {
+	info, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
